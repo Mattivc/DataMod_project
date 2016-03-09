7280601b21286519f750c90aa1cdc4d82e68098b
Matias Christensen <mattivc@gmail.com>
2016-03-09 11:22:30 +0100

    Updated project structure

--- Trening/src/TreningProgram.java
+++
@@ -1,7 +0,0 @@
-
-public class TreningProgram {
-
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/TreningProgram.java
@@ -0,0 +1,42 @@
+import java.sql.*;
+
+
+public class TreningProgram {
+
+    public static void main(String[] args) {
+        Connection con = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        String url = "jdbc:mysql://localhost:3306/Trening";
+        String user = "user";
+        String password = "12345678";
+
+        try {
+            con = DriverManager.getConnection(url, user, password);
+            st = con.createStatement();
+            rs = st.executeQuery("SELECT VERSION()");
+
+            if (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            try{
+                if (rs != null) {
+                    rs.close();
+                }
+                if (st != null) {
+                    st.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
