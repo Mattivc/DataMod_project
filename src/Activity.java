b6c36c3152e77f817ba748950c8bf67386341118
Tord Asnes <tord.aasnes@gmail.com>
2016-03-09 15:52:05 +0100

    Removing activity

--- src/Activity.java
+++ src/Activity.java
@@ -4,66 +4,43 @@ import java.sql.*;
 
 public class Activity {
 
-    public static void add(Connection con, String name, String description, Integer replacement, Integer group) {
+    public static boolean add(Connection con, String name, String description, Integer replacement, Integer group) {
         try {
             PreparedStatement post = con.prepareStatement("INSERT INTO ØVELSE (Navn, Beskrivelse, Erstatning, GruppeID) VALUES (?, ?, ?, ?)");
             post.setString(1, name);
             post.setString(2, description);
-            post.setNull(3, 0);
-            post.setNull(4, 0);
-            post.execute();
-
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-
-    }
-
-    public static void remove(Statement st) {
-
-    }
-
-    public static void edit(Statement st) {
-
-    }
-
-
-
-    public static void main(String[] args) {
-        Connection con = null;
-        Statement st = null;
-        ResultSet rs = null;
-        String url = "jdbc:mysql://localhost:3306/Trening?useSSL=false";
-        String user = "user";
-        String password = "12345678";
-
-        try {
-            con = DriverManager.getConnection(url, user, password);
-            add(con, "Benkd", "Press", null, null);
-
-        } catch (SQLException var16) {
-            var16.printStackTrace();
-        } finally {
-            try {
-                if(rs != null) {
-                    rs.close();
-                }
-
-                if(st != null) {
-                    st.close();
-                }
-
-                if(con != null) {
-                    con.close();
-                }
-            } catch (SQLException var15) {
-                var15.printStackTrace();
+            if (replacement == null) {
+                post.setNull(3, 0);
+            } else {
+                post.setInt(3, replacement.intValue());
             }
 
+            if (group == null) {
+                post.setNull(4, 0);
+            } else {
+                post.setInt(4, group.intValue());
+            }
+
+            post.execute();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return false;
         }
 
+        return true;
     }
 
+    public static boolean removeWhereNameLike(Connection con, String name) {
+        try {
+            PreparedStatement post = con.prepareStatement("DELETE FROM ØVELSE WHERE Navn LIKE ?");
+            post.setString(1, name);
+            post.execute();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return false;
+        }
 
+        return true;
+    }
 
 }
