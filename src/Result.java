9b98ad10eadca77ffb9f477fbb287c98d9b343b4
Jorgen Henrichsen <jh.henrichs@gmail.com>
2016-03-15 15:35:42 +0100

    Added constructor for Result

--- src/Result.java
+++ src/Result.java
@@ -4,4 +4,12 @@ import java.sql.*;
  * Created by jorgen on 15/03/16.
  */
 public class Result {
+
+    int activityID, exerciseID;
+
+    public Result(int activityID, int exerciseID) {
+        this.activityID = activityID;
+        this.exerciseID = exerciseID;
+    }
+
 }
